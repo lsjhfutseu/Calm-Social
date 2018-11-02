2aace4efb2b5a3eed06e81a0ce474b6920ec3c05
caobin <245420784@qq.com>
2018-11-02 21:43:59 +0800

    cb

---
+++ src/main/java/com/social/commonpojo/SimpleUser.java
@@ -0,0 +1,16 @@
+package com.social.commonpojo;
+
+public class SimpleUser {
+	private Integer id;
+
+	private String name;
+
+	public SimpleUser(Integer id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	
+
+}
